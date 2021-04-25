c534a2365460a4bd67c67b98ea109a8db5482c73
leyarotheconquerer <leyarotheconquerer@yahoo.com>
2021-04-24 20:33:25 -0500

    Add basic dragon which attacks
    The attacks just don't hit yet

---
+++ DwarvesDragons/Source/DwarvesDragons/Dragon/DragonAttack.cpp
@@ -0,0 +1,92 @@
+// Copyright 2021 Hazen Johnson
+
+
+#include "DragonAttack.h"
+
+#include "DwarvesDragons/Dwarf/DwarfHealth.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values for this component's properties
+UDragonAttack::UDragonAttack()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UDragonAttack::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UDragonAttack::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (_target)
+	{
+		FVector look = _target->GetActorLocation() - _actor->GetActorLocation();
+		look.Z = 0;
+		_actor->SetActorRotation(look.Rotation());
+	}
+	// ...
+}
+
+void UDragonAttack::Initialize(AActor* actor, TSubclassOf<AActor> attack, FAttackEvent attackEvent, float range, float rate, bool debug)
+{
+	_actor = actor;
+	_attack = attack;
+	_attackEvent = attackEvent;
+	_rate = rate;
+	_range = range;
+	_debug = debug;
+}
+
+bool UDragonAttack::Detect(AActor* other)
+{
+	if (other->FindComponentByClass(UDwarfHealth::StaticClass()))
+	{
+		_enemies.Add(other);
+		if (!_target) { _target = other; }
+		GetWorld()->GetTimerManager().SetTimer(_attackTimer, this, &UDragonAttack::StartAttack, _rate, false);
+		return true;
+	}
+	return false;
+}
+
+void UDragonAttack::StartAttack()
+{
+	TSet<AActor*> remove;
+	for(AActor* enemy : _enemies)
+	{
+		if (FVector::DistSquared(_actor->GetActorLocation(), enemy->GetActorLocation()) >= _range * _range)
+		{
+			remove.Add(enemy);
+			if (_target == enemy) { _target = nullptr; }
+		}
+	}
+	for (AActor* enemy : remove) { _enemies.Remove(enemy); }
+	if (_enemies.Num() > 0)
+	{
+		if (!_target) { _target = _enemies.Array()[0]; }
+		_attackEvent.Execute(_target->GetActorLocation());
+	}
+}
+
+void UDragonAttack::Attack()
+{
+	FVector location = GetComponentLocation();
+	FRotator rotation = GetComponentRotation();
+	GetWorld()->SpawnActor(_attack, &location, &rotation);
+	GetWorld()->GetTimerManager().SetTimer(_attackTimer, this, &UDragonAttack::StartAttack, _rate, false);
+}
+
---
+++ DwarvesDragons/Source/DwarvesDragons/Dragon/DragonAttack.h
@@ -0,0 +1,50 @@
+// Copyright 2021 Hazen Johnson
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "DragonAttack.generated.h"
+
+UDELEGATE(BlueprintAuthorityOnly)
+DECLARE_DYNAMIC_DELEGATE_OneParam(FAttackEvent, FVector, target);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class DWARVESDRAGONS_API UDragonAttack : public USceneComponent
+{
+	GENERATED_BODY()
+
+private:
+	AActor* _actor = nullptr;
+	FAttackEvent _attackEvent;
+	FTimerHandle _attackTimer;
+	AActor* _target = nullptr;
+	TSet<AActor*> _enemies = {};
+	TSubclassOf<AActor> _attack;
+	float _rate;
+	float _range;
+	bool _debug = false;
+
+public:	
+	// Sets default values for this component's properties
+	UDragonAttack();
+
+	UFUNCTION(BlueprintCallable, Category="Initialize")
+	void Initialize(AActor* actor, TSubclassOf<AActor> attack, FAttackEvent attackEvent, float range, float rate = 5.f, bool debug = false);
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable, Category="Attack")
+	bool Detect(AActor* other);
+
+	UFUNCTION(BlueprintCallable, Category="Attack")
+	void Attack();
+
+	void StartAttack();
+};
